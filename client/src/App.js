56e8789a92546f61746803dd89715631dc4daf69
jayhan1109 <jungho1109@gmail.com>
2021-06-16 16:30:38 -0700

    update firebase auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,28 @@
-import React, { Fragment, Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Home from './components/pages/Home';
-import Navbar from './components/layout/Navbar';
-import MyPage from './components/pages/MyPage';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./components/pages/Home";
+import Navbar from "./components/layout/Navbar";
+import MyPage from "./components/pages/MyPage";
+import Login from "./components/pages/Login";
 
-function App() {
-
+const App = () => {
   return (
-    <Component>
-      <div className='main'>
+    <Fragment>
+      <div className="main">
         <Router>
-          <Fragment>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/:id' component={MyPage} />
-              </Switch>
-            </div>
-          </Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/:id" component={MyPage} />
+            </Switch>
+          </div>
         </Router>
       </div>
-    </Component>
+    </Fragment>
   );
-}
+};
 
 export default App;
--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import firebase from 'firebase';
-import { firebaseConfig, uiConfig } from '../../firebase';
+import React, { Fragment } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtIRZv9COOErnAL51Z9BMWdB3-x0c4Te8",
+  authDomain: "ehducation-3.firebaseapp.com",
+  databaseURL: "https://ehducation-3-default-rtdb.firebaseio.com",
+  projectId: "ehducation-3",
+  storageBucket: "ehducation-3.appspot.com",
+  messagingSenderId: "786937482692",
+  appId: "1:786937482692:web:2a3bf3d8d79e87ca8aa891",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: "/",
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+};
 
 const Login = () => {
+  return (
+    <Fragment>
+      s<h1>Welcome to My Awesome App</h1>
+      <div id="firebaseui-auth-container"></div>
+      <div id="loader">Loading...</div>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </Fragment>
+  );
+};
 
-    return (
-        <div>
-            <h1>Welcome to My Awesome App</h1>
-            <div id="firebaseui-auth-container"></div>
-            <div id="loader">Loading...</div>
-            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-        </div>
-    )
-}
-
-export default Login
+export default Login;
